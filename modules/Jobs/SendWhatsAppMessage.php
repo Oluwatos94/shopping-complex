8e65d9dc0c3a2e6136be140798b0b5708d789d42
Oluwatos94 <akinbowat@gmail.com>
2026-04-07 22:30:49 +0100

    small fix

--- modules/Jobs/SendWhatsAppMessage.php
+++ modules/Jobs/SendWhatsAppMessage.php
@@ -31,8 +31,8 @@ class SendWhatsAppMessage implements ShouldQueue
 
     public function handle(): void
     {
-        $phoneNumberId = config('services.whatsapp.phone_number_id');
-        $accessToken = config('services.whatsapp.access_token');
+        $phoneNumberId = trim((string) config('services.whatsapp.phone_number_id'), "\"' \t\n\r");
+        $accessToken = trim((string) config('services.whatsapp.access_token'), "\"' \t\n\r");
 
         $response = Http::withToken((string) $accessToken)
             ->post("https://graph.facebook.com/v19.0/{$phoneNumberId}/messages", $this->payload);
